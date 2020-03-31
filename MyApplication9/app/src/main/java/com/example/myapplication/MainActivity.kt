c3afd00187fb90789244ce5a3f9f73cd979e46db
linas19 <linas.jonika1@gmail.com>
2020-03-31 20:20:41 +0300

    1st app

---
+++ MyApplication9/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        textView.text = count.toString()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> resetClicked()
+            else -> super.onOptionsItemSelected(item)
+        }
+            }
+    fun buttonClicked(view: View){
+        count += 1
+        textView.text = count.toString()
+
+    }
+    fun resetClicked():Boolean{
+        count = 0
+        textView.text = count.toString()
+        return true
+    }
+}
